1cefd93ec7af3c01395e8f1d352963ba375a78d6
smidrkal <josef@smidrkal.cz>
2023-10-09 17:55:39 +0200

    ukol1 zaklad

---
+++ ukol-01.py
@@ -0,0 +1,7 @@
+# https://github.com/JankaMarschalkova/python-podzim-pondeli-2023/blob/main/ukoly/ukol-01.md
+
+jmeno=input("Zadej jméno: ")
+print(f"{jmeno}@czechitas.cz")
+
+print("------")
+jmeno=input("Zadej jméno a příjmení: ")
